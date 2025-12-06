b388513c4733f047d1b6bcd700027884ab7664da
cabdixakim <cabdixakimsalan@gmail.com>
2025-12-06 23:28:01 +0200

    removing vendor installig package from repo

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc514d8f7b9fc5970bdd94287905ef584
+class ComposerStaticInitb29015b13df12692adf14609388ea840
 {
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
@@ -7181,9 +7181,9 @@ class ComposerStaticInitc514d8f7b9fc5970bdd94287905ef584
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc514d8f7b9fc5970bdd94287905ef584::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc514d8f7b9fc5970bdd94287905ef584::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInitc514d8f7b9fc5970bdd94287905ef584::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb29015b13df12692adf14609388ea840::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb29015b13df12692adf14609388ea840::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitb29015b13df12692adf14609388ea840::$classMap;
 
         }, null, ClassLoader::class);
     }
